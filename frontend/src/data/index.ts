db727de885ce73b30d1720b2a67bb2cc28e19899
Marshall Thompson <marshallthomp@gmail.com>
2021-02-24 23:14:56 -0500

    add data types, index export data

---
+++ frontend/src/data/index.ts
@@ -0,0 +1,4 @@
+export { groomsmanData, bridesmaidsData } from './people';
+export { registryData } from './gift-registry';
+
+export { FeaturedPerson, GiftRegisry } from './types';
---
+++ frontend/src/data/types.ts
@@ -0,0 +1,12 @@
+export type GiftRegisry = {
+  storeName: string;
+  registryUrl: string;
+  logo?: string;
+};
+
+export type FeaturedPerson = {
+  name: string;
+  role: string;
+  bio: string;
+  imgUrl: string;
+};
