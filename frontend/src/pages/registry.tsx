18a21641d0aa61a1ea301f25bf21c69c7ce148b5
Marshall Thompson <marshallthomp@gmail.com>
2021-02-24 23:23:51 -0500

    use registry data

--- frontend/src/pages/registry.tsx
+++ frontend/src/pages/registry.tsx
@@ -3,18 +3,23 @@ import { PageProps } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { Link } from "../components/navigation";
+import { registryData } from '../data';
 
 const OurStory: FC<PageProps<{}>> = () => (
   <Layout>
     <SEO title="Registry" />
-    <h1 className="font-headings text-6xl text-center">Registry</h1>
-    <p>Links to our registries will go here</p>
-    <ul className="mt-4">
-      <li>Bed Bath Beyond</li>
-      <li>Amazon</li>
-      <li>Crate and Barrel</li>
-      <li>Honeymoon fund?</li>
-    </ul>
+    <div className="container mx-auto">
+
+      <p>Links to our registries will go here</p>
+      <ul className="mt-4">
+        {registryData.map(registry => (
+          <li key={registry.storeName}>
+            <Link href={registry.registryUrl}>{registry.storeName}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   </Layout>
 );
 
