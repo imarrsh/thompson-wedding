3b784d462b493b78fc08a1346da5885a268daa51
Marshall Thompson <marshallthomp@gmail.com>
2021-04-03 16:20:13 -0400

    add svg ornaments

---
+++ frontend/src/components/icons/ornaments/index.ts
@@ -0,0 +1,4 @@
+export { LeafyCrown } from './LeafyCrown';
+export { LeafyCrownFlat } from './LeafyCrownFlat';
+export { LeafyJewelCrown } from './LeafyJewelCrown';
+export { SquigglyLinePeakUp } from './SquigglyLinePeakUp';
