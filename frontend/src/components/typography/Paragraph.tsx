3c97b03f1127b096963512e17dc2d203a6c5c142
Marshall Thompson <marshallthomp@gmail.com>
2021-06-13 11:34:05 -0400

    add class keys for props

--- frontend/src/components/typography/Paragraph.tsx
+++ frontend/src/components/typography/Paragraph.tsx
@@ -4,9 +4,22 @@ import { TypographyBase } from './TypographyBase';
 
 type ParagraphProps = {
   weight?: string;
-  spacing?: number;
-  leading?: number;
+  spacing?: keyof typeof spacingKey;
+  leading?: keyof typeof leadingKey;
 }
+
+// tailwind/purgecss needs full classNames in prod, so this makes sure theyre there
+const spacingKey = {
+  4: 'my-4',
+  8: 'my-8',
+  12: 'my-12'
+};
+
+const leadingKey = {
+  4: 'leading-4',
+  8: 'leading-8',
+  12: 'leading-12'
+};
 
 export const Paragraph: FC<ParagraphProps> = ({
   weight = 'font-light',
@@ -14,11 +27,12 @@ export const Paragraph: FC<ParagraphProps> = ({
   leading = 8,
   children
 }) => {
+
   return (
     <TypographyBase element="p" classes={clsx(
       'font-sans', 
-      `my-${spacing}`, 
-      `leading-${leading}`,
+      spacingKey[spacing], 
+      leadingKey[leading],
       weight
     )}>
       {children}
