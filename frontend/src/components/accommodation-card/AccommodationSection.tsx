4e9db9c750f446be70e74226bd319fe5d0a522f9
Marshall Thompson <marshallthomp@gmail.com>
2021-06-13 11:34:47 -0400

    change to heading

--- frontend/src/components/accommodation-card/AccommodationSection.tsx
+++ frontend/src/components/accommodation-card/AccommodationSection.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { AccommodationCard } from '.';
-import { TertiaryHeading } from '../typography';
+import { Heading } from '../typography';
 import { Location } from '../../data';
 
 type AccommodationSectionProps = {
@@ -11,8 +11,8 @@ type AccommodationSectionProps = {
 export const AccommodationSection: FC<AccommodationSectionProps> = ({category, list}) => {
   return list.length ? (
     <>
-      <TertiaryHeading textSize="text-4xl" className="font-script lowercase mt-2">{category}</TertiaryHeading>
-      <ul className="mt-4 grid sm:grid-cols-2 lg:grid-cols-3 grid-cols-1 grid-rows-2 gap-8">
+      <Heading level={3} size="xl" fontFamilyStyle="script" classes="lowercase mt-6">{category}</Heading>
+      <ul className="mt-4 grid sm:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-8">
         {list.map(a => 
           <li key={a.name}>
             <AccommodationCard accommodation={a}/>
