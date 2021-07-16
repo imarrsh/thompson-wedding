e4bb29860871bd2b575f2d8ad1978b813571bd4c
Marshall Thompson <marshallthomp@gmail.com>
2021-07-16 15:02:56 -0400

    fix icon

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         background_color: `#fafafa`,
         theme_color: `#fafafa`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
