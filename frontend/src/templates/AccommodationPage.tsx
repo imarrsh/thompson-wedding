c5adddc2354a16dd0870b537cb68588d92cf1b0a
Marshall Thompson <marshallthomp@gmail.com>
2021-07-28 17:48:24 -0400

    add link serializer to capture outbound links from PortableText

--- frontend/src/templates/AccommodationPage.tsx
+++ frontend/src/templates/AccommodationPage.tsx
@@ -8,6 +8,7 @@ import { StyledBlockContent } from '../components/block-content';
 import { SquigglyLinePeakUp } from '../components/icons/ornaments';
 import { Heading } from '../components/typography';
 import { MapLinkedAddress } from '../components/map-linked-address';
+import { Link } from '../components/navigation';
 
 
 type AccommodationPageProps = {
@@ -21,6 +22,22 @@ const AccommodationPage: FC<AccommodationPageProps> = ({
   data
 }) => {
   const { location } = data;
+  const blockSerializers = {
+    // @ts-expect-error - defaultSerializers isn't expected on BlockContent
+    ...BlockContent.defaultSerializers, 
+    ...{
+      marks: {
+      // @ts-expect-error - defaultSerializers isn't expected on BlockContent
+        ...BlockContent.defaultSerializers.marks,
+        ...{ 
+          link : (props: any) => {
+            const { mark, children, ...rest } = props;
+            return (<Link href={mark.href} {...rest}>{children}</Link>)
+          } 
+        }
+      }
+    }
+  }
   return (
     <section className="p-4 max-w-4xl mx-auto">
       <SEO title={`Accommodations: ${location.name}`} />
@@ -46,8 +63,7 @@ const AccommodationPage: FC<AccommodationPageProps> = ({
             <MapLinkedAddress location={location} />
           </section>
           {location.description !== null
-            // @ts-expect-error - defaultSerializers isn't expected on BlockContent
-            ? <StyledBlockContent blocks={location.description} serializers={BlockContent.defaultSerializers} />
+            ? <StyledBlockContent blocks={location.description} serializers={blockSerializers} />
             : null
           }
         </article>
