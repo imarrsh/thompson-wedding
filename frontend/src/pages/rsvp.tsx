351f734a007d7153e16f7017014da1fb6f439b6f
Marshall Thompson <marshallthomp@gmail.com>
2021-09-07 21:07:56 -0400

    update rsvp with passed deadline

--- frontend/src/pages/rsvp.tsx
+++ frontend/src/pages/rsvp.tsx
@@ -7,58 +7,21 @@ import type { GiftRegistry } from "../data";
 import { Button } from "../components/buttons";
 import { Link } from "../components/navigation/Link";
 
-interface RsvpPageProps {
-  registries: {
-    // just borrowing this type
-    nodes: GiftRegistry[]
-  }
-}
-
-const Rsvp = (props: PageProps<RsvpPageProps>) => {
-  
-  const {
-    data: {
-      registries: { nodes: sites }
-    }
-  } = props;
-
-  const site = sites[0];
-
+const Rsvp = () => {
   return (
     <section className="p4 mx-auto">
       <div className="container mx-auto px-4 max-w-4xl flex-grow">
         <SEO title="RSVP" />
         <SquigglyUpsidedownHeart className="h-24 w-24 text-sageGreen-500 fill-current mx-auto"/>
-        <Paragraph textAlign="center">We're so excited for you to join us! Follow the link below to RSVP via the Wedding Wire RSVP page.</Paragraph>
-        <Paragraph textAlign="center">For those bringing a guest, please Accept or Deny their RSVP. If Accepting, include their first and last name in the note section as we will need their name. Thank you!</Paragraph>
-        <div className="text-center">
-          <Link href={site.url}>
-            <Button variant="fill">RSVP at {site.name}</Button>
-          </Link>
-        </div>
         <Paragraph textAlign="center">
-          <em className="text-gray-500">If you are having trouble finding your name, make sure there are no trailing spaces in the text boxes and that the spelling matches the invitation. If you are still having trouble, reach out to one of us with your response and we'll help get you settled!</em>
-        </Paragraph>      
+          <strong className="font-bold">The deadline to RSVP has passed.</strong>
+        </Paragraph>
+        <Paragraph>
+          If you've RSVP'd, we're so excited for you to join us! 
+        </Paragraph>
       </div>
     </section>
   );
 };
 
 export default Rsvp;
-
-export const query = graphql`
-  query SingleRegistryQuery {
-    registries: allSanityRegistry (
-      filter: { 
-        name: {
-          in: ["Wedding Wire"] # grab wedding wire only - its not really a registry entry, but... 
-        }
-      }
-    ) {
-      nodes {
-        url
-        name
-      }
-    }
-  }
-`;
